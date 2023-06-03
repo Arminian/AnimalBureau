4924e2aae15f2f77e8c7397309e36b6d55cb81bc
Arminian <arminian@pm.me>
2023-06-03 21:31:29 +0300

    Complete project

--- AnimalBureau/MainMenu.Designer.cs
+++ AnimalBureau/MainMenu.Designer.cs
@@ -30,23 +30,12 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
             pictureBox1 = new PictureBox();
-            domainLabel = new Label();
-            kingdomLabel = new Label();
             phylumBox = new TextBox();
-            phylumLabel = new Label();
             classBox = new TextBox();
-            classLabel = new Label();
             orderBox = new TextBox();
-            orderLabel = new Label();
             familyBox = new TextBox();
-            familyLabel = new Label();
             genusBox = new TextBox();
-            genusLabel = new Label();
             speciesBox = new TextBox();
-            speciesLabel = new Label();
-            domaibBox = new ComboBox();
-            kingdomBox = new ComboBox();
-            dataGrid = new DataGridView();
             addButton = new Button();
             updateButton = new Button();
             deleteButton = new Button();
@@ -54,241 +43,149 @@
             pictureBox2 = new PictureBox();
             seachLabel = new Label();
             searchBox = new TextBox();
+            userGV = new DataGridView();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1 = new MenuStrip();
+            searchButton = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dataGrid).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)userGV).BeginInit();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Location = new Point(12, 27);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(248, 70);
+            pictureBox1.Size = new Size(332, 88);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
-            // domainLabel
-            // 
-            domainLabel.AutoSize = true;
-            domainLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            domainLabel.Location = new Point(16, 131);
-            domainLabel.Name = "domainLabel";
-            domainLabel.Size = new Size(65, 21);
-            domainLabel.TabIndex = 1;
-            domainLabel.Text = "Domain";
-            // 
-            // kingdomLabel
-            // 
-            kingdomLabel.AutoSize = true;
-            kingdomLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            kingdomLabel.Location = new Point(9, 183);
-            kingdomLabel.Name = "kingdomLabel";
-            kingdomLabel.Size = new Size(73, 21);
-            kingdomLabel.TabIndex = 3;
-            kingdomLabel.Text = "Kingdom";
-            // 
             // phylumBox
             // 
             phylumBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            phylumBox.Location = new Point(87, 231);
+            phylumBox.Location = new Point(35, 183);
             phylumBox.Name = "phylumBox";
-            phylumBox.Size = new Size(173, 29);
+            phylumBox.PlaceholderText = "Phylum";
+            phylumBox.Size = new Size(225, 29);
             phylumBox.TabIndex = 6;
-            // 
-            // phylumLabel
-            // 
-            phylumLabel.AutoSize = true;
-            phylumLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            phylumLabel.Location = new Point(18, 234);
-            phylumLabel.Name = "phylumLabel";
-            phylumLabel.Size = new Size(63, 21);
-            phylumLabel.TabIndex = 5;
-            phylumLabel.Text = "Phylum";
             // 
             // classBox
             // 
             classBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            classBox.Location = new Point(87, 282);
+            classBox.Location = new Point(35, 234);
             classBox.Name = "classBox";
-            classBox.Size = new Size(173, 29);
+            classBox.PlaceholderText = "Class";
+            classBox.Size = new Size(225, 29);
             classBox.TabIndex = 8;
-            // 
-            // classLabel
-            // 
-            classLabel.AutoSize = true;
-            classLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            classLabel.Location = new Point(35, 285);
-            classLabel.Name = "classLabel";
-            classLabel.Size = new Size(46, 21);
-            classLabel.TabIndex = 7;
-            classLabel.Text = "Class";
             // 
             // orderBox
             // 
             orderBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            orderBox.Location = new Point(87, 335);
+            orderBox.Location = new Point(35, 287);
             orderBox.Name = "orderBox";
-            orderBox.Size = new Size(173, 29);
+            orderBox.PlaceholderText = "Order";
+            orderBox.Size = new Size(225, 29);
             orderBox.TabIndex = 10;
-            // 
-            // orderLabel
-            // 
-            orderLabel.AutoSize = true;
-            orderLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            orderLabel.Location = new Point(30, 338);
-            orderLabel.Name = "orderLabel";
-            orderLabel.Size = new Size(51, 21);
-            orderLabel.TabIndex = 9;
-            orderLabel.Text = "Order";
             // 
             // familyBox
             // 
             familyBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            familyBox.Location = new Point(87, 388);
+            familyBox.Location = new Point(35, 340);
             familyBox.Name = "familyBox";
-            familyBox.Size = new Size(173, 29);
+            familyBox.PlaceholderText = "Family";
+            familyBox.Size = new Size(225, 29);
             familyBox.TabIndex = 12;
-            // 
-            // familyLabel
-            // 
-            familyLabel.AutoSize = true;
-            familyLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            familyLabel.Location = new Point(26, 391);
-            familyLabel.Name = "familyLabel";
-            familyLabel.Size = new Size(55, 21);
-            familyLabel.TabIndex = 11;
-            familyLabel.Text = "Family";
             // 
             // genusBox
             // 
             genusBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            genusBox.Location = new Point(87, 441);
+            genusBox.Location = new Point(35, 393);
             genusBox.Name = "genusBox";
-            genusBox.Size = new Size(173, 29);
+            genusBox.PlaceholderText = "Genus";
+            genusBox.Size = new Size(225, 29);
             genusBox.TabIndex = 14;
-            // 
-            // genusLabel
-            // 
-            genusLabel.AutoSize = true;
-            genusLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            genusLabel.Location = new Point(26, 444);
-            genusLabel.Name = "genusLabel";
-            genusLabel.Size = new Size(54, 21);
-            genusLabel.TabIndex = 13;
-            genusLabel.Text = "Genus";
             // 
             // speciesBox
             // 
             speciesBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            speciesBox.Location = new Point(87, 496);
+            speciesBox.Location = new Point(35, 448);
             speciesBox.Name = "speciesBox";
-            speciesBox.Size = new Size(173, 29);
+            speciesBox.PlaceholderText = "Species";
+            speciesBox.Size = new Size(225, 29);
             speciesBox.TabIndex = 16;
-            // 
-            // speciesLabel
-            // 
-            speciesLabel.AutoSize = true;
-            speciesLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            speciesLabel.Location = new Point(19, 499);
-            speciesLabel.Name = "speciesLabel";
-            speciesLabel.Size = new Size(62, 21);
-            speciesLabel.TabIndex = 15;
-            speciesLabel.Text = "Species";
-            // 
-            // domaibBox
-            // 
-            domaibBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            domaibBox.FormattingEnabled = true;
-            domaibBox.Items.AddRange(new object[] { "Bacteria", "Archaea", "Eukarya" });
-            domaibBox.Location = new Point(87, 128);
-            domaibBox.Name = "domaibBox";
-            domaibBox.Size = new Size(173, 29);
-            domaibBox.TabIndex = 17;
-            // 
-            // kingdomBox
-            // 
-            kingdomBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            kingdomBox.FormattingEnabled = true;
-            kingdomBox.Items.AddRange(new object[] { "Eubacteria", "Archaebacteria", "Protista", "Fungi", "Plantae", "Animalia" });
-            kingdomBox.Location = new Point(87, 180);
-            kingdomBox.Name = "kingdomBox";
-            kingdomBox.Size = new Size(173, 29);
-            kingdomBox.TabIndex = 18;
-            // 
-            // dataGrid
-            // 
-            dataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGrid.Location = new Point(277, 168);
-            dataGrid.Name = "dataGrid";
-            dataGrid.RowTemplate.Height = 25;
-            dataGrid.Size = new Size(601, 404);
-            dataGrid.TabIndex = 19;
             // 
             // addButton
             // 
             addButton.BackColor = Color.IndianRed;
             addButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             addButton.ForeColor = SystemColors.Window;
-            addButton.Location = new Point(277, 31);
+            addButton.Location = new Point(487, 31);
             addButton.Name = "addButton";
             addButton.Size = new Size(109, 51);
             addButton.TabIndex = 20;
             addButton.Text = "Add";
             addButton.UseVisualStyleBackColor = false;
+            addButton.Click += addButton_Click;
             // 
             // updateButton
             // 
             updateButton.BackColor = Color.IndianRed;
             updateButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             updateButton.ForeColor = SystemColors.WindowFrame;
-            updateButton.Location = new Point(415, 31);
+            updateButton.Location = new Point(625, 31);
             updateButton.Name = "updateButton";
             updateButton.Size = new Size(109, 51);
             updateButton.TabIndex = 21;
             updateButton.Text = "Update";
             updateButton.UseVisualStyleBackColor = false;
+            updateButton.Click += updateButton_Click;
             // 
             // deleteButton
             // 
             deleteButton.BackColor = Color.IndianRed;
             deleteButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             deleteButton.ForeColor = SystemColors.Window;
-            deleteButton.Location = new Point(553, 31);
+            deleteButton.Location = new Point(763, 31);
             deleteButton.Name = "deleteButton";
             deleteButton.Size = new Size(109, 51);
             deleteButton.TabIndex = 22;
             deleteButton.Text = "Delete";
             deleteButton.UseVisualStyleBackColor = false;
+            deleteButton.Click += deleteButton_Click;
             // 
             // clearButton
             // 
             clearButton.BackColor = Color.IndianRed;
             clearButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             clearButton.ForeColor = SystemColors.Window;
-            clearButton.Location = new Point(689, 31);
+            clearButton.Location = new Point(899, 31);
             clearButton.Name = "clearButton";
             clearButton.Size = new Size(109, 51);
             clearButton.TabIndex = 23;
             clearButton.Text = "Clear";
             clearButton.UseVisualStyleBackColor = false;
+            clearButton.Click += clearButton_Click;
             // 
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(821, 27);
+            pictureBox2.Location = new Point(1031, 27);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(57, 55);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox2.TabIndex = 24;
             pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
             // 
             // seachLabel
             // 
             seachLabel.AutoSize = true;
             seachLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            seachLabel.Location = new Point(287, 131);
+            seachLabel.Location = new Point(287, 146);
             seachLabel.Name = "seachLabel";
             seachLabel.Size = new Size(57, 21);
             seachLabel.TabIndex = 25;
@@ -297,16 +194,66 @@
             // searchBox
             // 
             searchBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            searchBox.Location = new Point(350, 128);
+            searchBox.Location = new Point(350, 143);
             searchBox.Name = "searchBox";
-            searchBox.Size = new Size(528, 29);
+            searchBox.Size = new Size(603, 29);
             searchBox.TabIndex = 26;
+            // 
+            // userGV
+            // 
+            userGV.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            userGV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            userGV.Location = new Point(287, 183);
+            userGV.Name = "userGV";
+            userGV.RowTemplate.Height = 25;
+            userGV.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            userGV.Size = new Size(801, 405);
+            userGV.TabIndex = 28;
+            userGV.CellContentClick += userGV_CellContentClick;
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(44, 20);
+            helpToolStripMenuItem.Text = "Help";
+            helpToolStripMenuItem.Click += helpToolStripMenuItem_Click;
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(52, 20);
+            aboutToolStripMenuItem.Text = "About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { helpToolStripMenuItem, aboutToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1100, 24);
+            menuStrip1.TabIndex = 29;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // searchButton
+            // 
+            searchButton.BackColor = Color.IndianRed;
+            searchButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            searchButton.ForeColor = SystemColors.Window;
+            searchButton.Location = new Point(959, 140);
+            searchButton.Name = "searchButton";
+            searchButton.Size = new Size(129, 37);
+            searchButton.TabIndex = 30;
+            searchButton.Text = "Search";
+            searchButton.UseVisualStyleBackColor = false;
+            searchButton.Click += searchButton_Click;
             // 
             // MainMenu
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(900, 600);
+            ClientSize = new Size(1100, 600);
+            Controls.Add(searchButton);
+            Controls.Add(userGV);
             Controls.Add(searchBox);
             Controls.Add(seachLabel);
             Controls.Add(pictureBox2);
@@ -314,30 +261,24 @@
             Controls.Add(deleteButton);
             Controls.Add(updateButton);
             Controls.Add(addButton);
-            Controls.Add(dataGrid);
-            Controls.Add(kingdomBox);
-            Controls.Add(domaibBox);
             Controls.Add(speciesBox);
-            Controls.Add(speciesLabel);
             Controls.Add(genusBox);
-            Controls.Add(genusLabel);
             Controls.Add(familyBox);
-            Controls.Add(familyLabel);
             Controls.Add(orderBox);
-            Controls.Add(orderLabel);
             Controls.Add(classBox);
-            Controls.Add(classLabel);
             Controls.Add(phylumBox);
-            Controls.Add(phylumLabel);
-            Controls.Add(kingdomLabel);
-            Controls.Add(domainLabel);
             Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "MainMenu";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Menu";
+            Load += MainMenu_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dataGrid).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)userGV).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -345,23 +286,12 @@
         #endregion
 
         private PictureBox pictureBox1;
-        private Label domainLabel;
-        private Label kingdomLabel;
         private TextBox phylumBox;
-        private Label phylumLabel;
         private TextBox classBox;
-        private Label classLabel;
         private TextBox orderBox;
-        private Label orderLabel;
         private TextBox familyBox;
-        private Label familyLabel;
         private TextBox genusBox;
-        private Label genusLabel;
         private TextBox speciesBox;
-        private Label speciesLabel;
-        private ComboBox domaibBox;
-        private ComboBox kingdomBox;
-        private DataGridView dataGrid;
         private Button addButton;
         private Button updateButton;
         private Button deleteButton;
@@ -369,5 +299,10 @@
         private PictureBox pictureBox2;
         private Label seachLabel;
         private TextBox searchBox;
+        private DataGridView userGV;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private MenuStrip menuStrip1;
+        private Button searchButton;
     }
 }
--- AnimalBureau/MainMenu.cs
+++ AnimalBureau/MainMenu.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using System.Data.SqlClient;
+
 namespace AnimalBureau
 {
     public partial class MainMenu : Form
@@ -5,6 +8,139 @@ namespace AnimalBureau
         public MainMenu()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Administrator\Documents\bureauDB.mdf;Integrated Security=True;Connect Timeout=30");
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("insert into UsrTable values('" + speciesBox.Text + "', '" + genusBox.Text + "','" + familyBox.Text + "','" + orderBox.Text + "','" + classBox.Text + "','" + phylumBox.Text + "')", connection);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("New Entry Added");
+                connection.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        void popul()
+        {
+            try
+            {
+                connection.Open();
+                string showQuery = "Select * from UsrTable";
+                SqlDataAdapter adapter = new SqlDataAdapter(showQuery, connection);
+                SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+                var dset = new DataSet();
+                adapter.Fill(dset);
+                userGV.DataSource = dset.Tables[0];
+                connection.Close();
+            }
+            catch (Exception tb)
+            {
+                MessageBox.Show("Error while loading table");
+            }
+        }
+
+        void search()
+        {
+            try
+            {
+                connection.Open();
+                string showQuery = "Select * from UsrTable where species='" + searchBox.Text + "'";
+                SqlDataAdapter adapter = new SqlDataAdapter(showQuery, connection);
+                SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+                var dset = new DataSet();
+                adapter.Fill(dset);
+                userGV.DataSource = dset.Tables[0];
+                connection.Close();
+            }
+            catch (Exception sr)
+            {
+                MessageBox.Show("Error while searching");
+            }
+        }
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+            popul();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            popul();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (speciesBox.Text == "")
+            {
+                MessageBox.Show("Enter species name in the text box 'species'");
+            }
+            else
+            {
+                connection.Open();
+                string deleteQuery = "Delete from UsrTable where species='" + speciesBox.Text + "'";
+                SqlCommand cmd = new SqlCommand(deleteQuery, connection);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Entry Deleted");
+                connection.Close();
+            }
+        }
+
+        private void userGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                phylumBox.Text = userGV.CurrentRow.Cells[5].Value.ToString();
+                classBox.Text = userGV.CurrentRow.Cells[4].Value.ToString();
+                orderBox.Text = userGV.CurrentRow.Cells[3].Value.ToString();
+                familyBox.Text = userGV.CurrentRow.Cells[2].Value.ToString();
+                genusBox.Text = userGV.CurrentRow.Cells[1].Value.ToString();
+                speciesBox.Text = userGV.CurrentRow.Cells[0].Value.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while selecting row");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            phylumBox.Text = "";
+            classBox.Text = "";
+            orderBox.Text = "";
+            familyBox.Text = "";
+            genusBox.Text = "";
+            speciesBox.Text = "";
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            search();
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            helpPage help = new helpPage();
+            help.Show();
+            this.Hide();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            aboutPage about = new aboutPage();
+            about.Show();
+            this.Hide();
         }
     }
 }
